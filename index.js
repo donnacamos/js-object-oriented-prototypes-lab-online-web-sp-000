3980b1952aaaf4af2cb17a019b15409bc21ab250
Donna Amos <donnacamos.developer@gmail.com>
2019-09-16 14:12:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+function BoardMember(name, homeState, training){
+   this.name = name
+   this.homeState = homeState
+   this.training = training
+}
+   BoardMember.protoType.veto = function(){
+     return 'No, I must disagree'
+   }
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
